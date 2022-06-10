b31970bdc5814c5d3890381dd1ca3daa8478bac7
DavideDiPietro <davide.dipietro3@gmail.com>
2022-06-10 13:58:14 +0200

    Inizio il progetto

--- public/js/deleteForm.js
+++ public/js/deleteForm.js
@@ -111,7 +111,7 @@ deleteForm.forEach(function (form) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\david\Desktop\Boolean\Esercizi\49 - Laravel One to many\laravel-auth\resources\js\deleteForm.js */"./resources/js/deleteForm.js");
+module.exports = __webpack_require__(/*! C:\Users\david\Desktop\Boolean\Esercizi\50 - Laravel Many to many\laravel-one-to-many\resources\js\deleteForm.js */"./resources/js/deleteForm.js");
 
 
 /***/ })
